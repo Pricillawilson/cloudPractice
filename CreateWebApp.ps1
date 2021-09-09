a6cd4bba9e062531ee0a0dc701a27e222314052e
PRICILLA W <41743198+Pricillawilson@users.noreply.github.com>
2021-09-09 12:55:24 +0530

    Update CreateWebApp.ps1
    commands added

--- CreateWebApp.ps1
+++ CreateWebApp.ps1
@@ -1,1 +1,13 @@
+#create variables
+$webappname = "mywebapp$(Get-Random)"
+$rgname = 'webapps3-dev-rg'
+$location = 'westus2'
 
+#create a resource group
+New-AzResourceGroup -Name $rgname -Location $location
+
+#create an App Service plan in S1 tier
+New-AzAppServicePlan -Name $webappname -Location $location -ResourceGroupName $rgname -Tier S1
+
+#Create a web app
+New-AzWebApp -Name $webappname -Location $location -AppServicePlan $webappname -ResourceGroupName $rgname
